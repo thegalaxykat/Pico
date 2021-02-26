61b272fc7f86a84e926ed85ed5cac12a3564bc54
Kat <60857666+thegalaxykat@users.noreply.github.com>
2021-02-26 15:00:05 -0500

    fancy button

--- Button.py
+++ Button.py
@@ -5,8 +5,21 @@ machine.Pin(25, machine.Pin.OUT).value(1)
 
 # sets the pin as INPUT and a PULL DOWN resistor (meaning that the non-pressed value is 0) 
 button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
+led_red = machine.Pin(15, machine.Pin.OUT)
+led_blue = machine.Pin(13, machine.Pin.OUT)
+
+times_pressed = 0
 
 while True:
     if button.value() == 1:
-        print("You see a button, you push the button. Simple as that.")
-        utime.sleep(1)
+        times_pressed += 1
+        if(times_pressed % 2) != 0:
+            print("red led")
+            led_blue.value(0)
+            led_red.value(1)
+            utime.sleep(.5)
+        else:
+            print("blue led")
+            led_blue.value(1)
+            led_red.value(0)
+            utime.sleep(.5)
