288d6455a6cdbc80022af3a6ea8c671440969eb3
Kat Canavan <Katcanavan@gmail.com>
2021-04-13 13:50:50 -0400

    Ch 8 potentiometer and temp sensor

---
+++ MicroPython/Temperature.py
@@ -0,0 +1,14 @@
+import machine
+import utime
+
+sensor_temp = machine.ADC(4) #temp sensor in RP2040
+conversion_factor = 3.3/(65535)
+
+while True:
+	reading = sensor_temp.read_u16() * conversion_factor
+	temperatureC = 27 - (reading - .706)/.001721 #converts to degrees C
+
+	temperatureF = (temperatureC * 9/5) + 32 
+
+	print(temperatureF)
+	utime.sleep(2)
