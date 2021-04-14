bde6180c0655b14c6c1d0d4be7a5c017d167d5dd
Kat Canavan <Katcanavan@gmail.com>
2021-04-14 09:36:47 -0400

    I2C test

--- MicroPython/LCD_I2C.py
+++ MicroPython/LCD_I2C.py
@@ -5,6 +5,7 @@ scl = machine.Pin(1) # clock wire
 i2c = machine.I2C(0, sda=sda, scl=scl, freq=400000)
 
 # sending data:
+
 # Note: 114 refers to the address of the screen and \x indicates a hexadecimal string
 i2c.writeto(114, '\x7C') # 7C enters command mode
 i2c.writeto(114, '\x2D') # 2D clears the screen and resets the cursor to the beginning
